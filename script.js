d0cb3f72004c107372745b9008da40208c4512e7
Suparna62 <suparnachaudhary34@gmail.com>
2025-07-13 11:33:06 +0530

    💡 Update index.html for portfolio

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple alert for contact interaction
+document.getElementById("contact").addEventListener("click", function () {
+  alert("Thanks for checking my portfolio. Let's connect soon!");
+});
